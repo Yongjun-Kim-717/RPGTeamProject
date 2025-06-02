9a465f041d365a2deb3d16e47179ca5814257adc
Yongjun-Kim-717 <rladydwns717@gmail.com>
2025-06-02 14:20:45 +0900

    123

---
+++ Assets/@Scripts/Manager/CutSceneManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class CutSceneManager : Singleton<CutSceneManager>, IDeactivateObject
+{
+    private GameObject _cutScene;
+
+    protected override void Initialize()
+    {
+        base.Initialize();
+        _cutScene = Instantiate(ObjectManager.Instance.GoblinKingCutScene);
+    }
+
+    public void Deactivate()
+    {
+        _cutScene.SetActive(false);
+    }
+    public void PlayCutScene()
+    {
+        _cutScene?.SetActive(true);
+        _cutScene.GetComponent<CutSceneController>().PlayCutScene();
+    }
+}
